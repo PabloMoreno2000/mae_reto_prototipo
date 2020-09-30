ebbd4c5c0aaebdcce67e8d767d8c68ad1d28a465
PabloMoreno2000 <pamta2000@gmail.com>
2020-09-29 21:45:06 -0500

    Added encanched auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,47 +1,71 @@
 const express = require("express");
 const router = express.Router();
-const Mae = require("../../models/Mae");
-const Student = require("../../models/Student");
-const Student = require("../../models/User");
-
-// By now this is a prototype, we'll be working just with one user
-let currentUser = {};
-let isMae = false;
+const User = require("../../models/User");
 
 // @route  POST api/auth
 // @desct  Authenticate user & save the info
 // @access Public/non-authentication/no-token
-router.post("/", async (req, res) => {
-  try {
+router.post(
+  "/",
+  [
+    check("matricula", "Favor de ingresar matrícula").isEmail(),
+    check("password", "Favor de ingresar contraseña").exists(),
+  ],
+  async (req, res) => {
+    // The check is done with the second parameter of above within []
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
     const { matricula, password } = req.body;
-    let user = await Mae.findOne({ matricula });
-    if (user) {
-      isMae = true;
-    } else {
-      user = await Student.findOne({ matricula });
+
+    try {
+      // If there's a user with that id
+      let user = await User.findOne({ matricula });
       if (!user) {
         return res
           .status(400)
-          .json({ errors: [{ msg: "Invalid credentials" }] });
+          .json({ errors: [{ msg: "Credenciales inválidas" }] });
       }
-      isMae = false;
-    }
 
-    if (password == user.password) {
-      currentUser = user;
+      // Check if the hash version of a non-encrypted text equals to a hash
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Credenciales inválidas" }] });
+      }
+
       const payload = {
-        user,
-        isMae,
+        user: {
+          id: user.id, // Moongose gets the id of the database
+        },
       };
-      res.send(payload);
-    } else {
-      res.status(400).json({ errors: [{ msg: "Invalid credentials" }] });
+
+      // Generate a JSON Web Token (encrypted payload with signature)
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"), // encryption key
+        { expiresIn: 360000 },
+        // Callback
+        (err, token) => {
+          // If there's an error, throw it
+          if (err) throw err;
+          // Else, set in the json of the response this web token with
+          // the user id that can be used for authentication after sign up
+          res.json({ token });
+        }
+      );
+
+      // res.send("User registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
     }
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server error");
   }
-});
+);
 
 // Get info of authenticated user
 router.get("/", (req, res) => {
