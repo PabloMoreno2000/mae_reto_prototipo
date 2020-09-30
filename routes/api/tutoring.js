221e2cf0c729dfead0f710303a282ced7eef20d4
PabloMoreno2000 <pamta2000@gmail.com>
2020-09-29 23:50:17 -0500

    Completed method to register a tutoring session

--- routes/api/tutoring.js
+++ routes/api/tutoring.js
@@ -1,37 +1,56 @@
+const mongoose = require("mongoose");
 const express = require("express");
-const auth = require("../../middleware/auth");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
 const User = require("../../models/User");
 const TS = require("../../models/TutoringSession");
+const { Mongoose } = require("mongoose");
 
+// TODO: Change to private, just student(receives) can do this
+// TODO: No user can register a session with itself
 // @route  POST api/tutoring
 // @desct  Create a tutoring session
-// @access Private
-router.post("/", auth, async (req, res) => {
-  try {
-    // the id is the mongo id, NOT the student/tutor id
-    const { userIdGives, userIdReceives } = req.body;
-    const tutor = await User.findById({ userIdGives });
-    const student = await User.findById({ userIdReceives });
+// @access Public
+router.post(
+  "/",
+  [
+    check("userIdGives", "Falta MAE").not().isEmpty(),
+    check("userIdReceives", "Falta asesorado").not().isEmpty(),
+  ],
+  async (req, res) => {
+    try {
+      // Check for errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+        });
+      }
 
-    // Both have to exist and tutor has to be a Mae
-    if (!tutor || !student || !tutor.isMae) {
-      return res.status(400).send("Bad Request");
+      // the id is the mongo id, NOT the student/tutor id
+      let { userIdGives, userIdReceives } = req.body;
+      const tutor = await User.findById(userIdGives);
+      const student = await User.findById(userIdReceives);
+
+      // Both have to exist and tutor has to be a Mae
+      if (!tutor || !student || !tutor.isMae) {
+        return res.status(400).send("Bad Request");
+      }
+
+      // Save the tutoring session and send it in response
+      const sessionInfo = {
+        gives: userIdGives,
+        receives: userIdReceives,
+      };
+      session = new TS(sessionInfo);
+      await session.save();
+      res.json(session);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
     }
-
-    // Save the tutoring session and send it in response
-    const sessionInfo = {
-      gives: userIdGives,
-      receives: userIdReceives,
-    };
-    session = new TS(sessionInfo);
-    await session.save();
-    res.json(session);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server error");
   }
-});
+);
 
 module.exports = router;
