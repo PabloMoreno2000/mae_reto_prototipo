2ffeadae71d5ca1ac3627467b78680359af3de38
PabloMoreno2000 <pamta2000@gmail.com>
2020-09-29 21:49:32 -0500

    Added require consts for file

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const auth = require("../../middleware/auth");
 const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator/check");
 
 // @route  POST api/auth
 // @desct  Authenticate user & save the info
